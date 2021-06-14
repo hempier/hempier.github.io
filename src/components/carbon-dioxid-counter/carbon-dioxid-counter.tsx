157dee664c61800cf2bdbfbb468b7b5a7f260544
m.riabko <m.riabko@pin-up.team>
2021-06-14 21:36:16 +0300

    Minor bugs fixes.

--- src/components/carbon-dioxid-counter/carbon-dioxid-counter.tsx
+++ src/components/carbon-dioxid-counter/carbon-dioxid-counter.tsx
@@ -1,26 +1,19 @@
 import * as React from "react"
 import "./carbon-dioxid-counter.scss"
 
-const InformationSection = ({ name, title, description, ctaText, image }) => {
-  const informationSectionImage = {
-    backgroundImage: `url(${image.file.url})`,
-  }
+const CarbonDioxidCounter = ({ name, title, description, ctaText, image }) => {
 
   return (
-    <section className="information-section">
-      <div className="information-section__inner-wrap container">
-        <div className="information-section__image col-6" style={informationSectionImage} />
-        <div className="information-section__text">
-          <div className="information-section__subtitle subtitle-2">{name}</div>
-          <h2 className="information-section__title">{title}</h2>
-          <p className="information-section__description">
-              {description.childMarkdownRemark.rawMarkdownBody}
-          </p>
-          {ctaText ? <a href="" className="information-section__cta cta-link">{ctaText}</a> : null}
+    <section className="carbon-counter container">
+      <div className="carbon-counter__inner-wrap row">
+        <div className="carbon-counter__subtitle subtitle-2">{name}</div>
+        <h2 className="carbon-counter__title">{title}</h2>
+        <div className="carbon-counter__content">
+
         </div>
       </div>
     </section>
   )
 }
 
-export default InformationSection
+export default CarbonDioxidCounter
